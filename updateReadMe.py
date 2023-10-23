4c2d8ea53c639b939204e5c7531a9ad27e190c9c
혜정 <jung5318@gmail.com>
2023-10-23 23:45:19 +0900

    update READ.ME

--- updateReadMe.py
+++ updateReadMe.py
@@ -32,17 +32,32 @@ markdown_text = """![header](https://capsule-render.vercel.app/api?type=waving&c
 
 ## 🚧 TECH STACK 🚧
 
+<div style="display:flex; flex-direction:column; align-items:flex-start;">
+
 ### 💡 Used as the main
 
-  <img src='https://img.shields.io/badge/node-518d7d?style=flat&logo=Node.js&logoColor=white'> <img src='https://img.shields.io/badge/Express-f8ca8f?style=flat&logo=express&logoColor=white'> <img src='https://img.shields.io/badge/JavaScript-ffcc00?style=flat&logo=javascript&logoColor=white'> <img src='https://img.shields.io/badge/php-04558A?style=flat&logo=php&logoColor=white'> <img src="https://img.shields.io/badge/MySql-586fab?style=flat&logo=mysql&logoColor=white"/> <img src="https://img.shields.io/badge/redis-d65353?style=flat&logo=redis&logoColor=white"/>
+<div>
+  <img src='https://img.shields.io/badge/node-518d7d?style=flat&logo=Node.js&logoColor=white'>
+  <img src='https://img.shields.io/badge/Express-f8ca8f?style=flat&logo=express&logoColor=white'>
+  <img src='https://img.shields.io/badge/JavaScript-ffcc00?style=flat&logo=javascript&logoColor=white'>
+  <img src='https://img.shields.io/badge/php-04558A?style=flat&logo=php&logoColor=white'>
+  <img src="https://img.shields.io/badge/MySql-586fab?style=flat&logo=mysql&logoColor=white"/>
+  <img src="https://img.shields.io/badge/redis-d65353?style=flat&logo=redis&logoColor=white"/>
+</div>
 
 ### 💡 Used as least once
 
-  <img src="https://img.shields.io/badge/Docker-4465c0?style=flat&logo=docker&logoColor=white"/> <img src="https://img.shields.io/badge/MongoDB-21aca9?style=flat&logo=mongodb&logoColor=white"/> <img src="https://img.shields.io/badge/Vue-01B257?style=flat&logo=vue.js&logoColor=white"/> <img src="https://img.shields.io/badge/html-fe8d6f?style=flat&logo=html5&logoColor=white"/>
-
+<div>
+  <img src="https://img.shields.io/badge/Amazon AWS-232F3E?style=flat&logo=amazon aws&logoColor=white"> 
+  <img src="https://img.shields.io/badge/Docker-4465c0?style=flat&logo=docker&logoColor=white"/>
+  <img src="https://img.shields.io/badge/MongoDB-21aca9?style=flat&logo=mongodb&logoColor=white"/>
+  <img src="https://img.shields.io/badge/Vue-01B257?style=flat&logo=vue.js&logoColor=white"/>
+  <img src="https://img.shields.io/badge/html-fe8d6f?style=flat&logo=html5&logoColor=white"/>
+</div>
 </br>
 </br>
 
+</div>
 <!-- [![Readme Card](https://github-readme-stats.vercel.app/api/pin/?username=yoonheyjung&theme=tokyonight&repo=badwords-ko)](https://github.com/yoonheyjung/badwords-ko) -->
 
 [![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=yoonheyjung&theme=onedark&layout=compact)](https://github.com/yoonheyjung/badwords-ko)
